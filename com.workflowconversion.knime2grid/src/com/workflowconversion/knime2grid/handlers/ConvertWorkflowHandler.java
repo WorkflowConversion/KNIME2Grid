102a831bc5613ae32656b37cb9e8ddeff60f7397
Luis de la Garza <delagarza@informatik.uni-tuebingen.de>
2018-03-08 17:22:53 +0100

    No longer using action/actionSet, which has been deprecated for a long time now. Instead, we are now using commands.

---
+++ com.workflowconversion.knime2grid/src/com/workflowconversion/knime2grid/handlers/ConvertWorkflowHandler.java
@@ -0,0 +1,63 @@
+package com.workflowconversion.knime2grid.handlers;
+
+import org.apache.commons.lang.Validate;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.knime.workbench.editor2.WorkflowEditor;
+
+import com.workflowconversion.knime2grid.KnimeWorkflowExporterActivator;
+import com.workflowconversion.knime2grid.export.workflow.KnimeWorkflowExporterProvider;
+import com.workflowconversion.knime2grid.ui.wizard.WorkflowExportWizard;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * 
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class ConvertWorkflowHandler extends AbstractHandler {
+
+    private static final int SIZING_WIZARD_WIDTH = 470;
+    private static final int SIZING_WIZARD_HEIGHT = 550;
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+	final IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+	Validate.notNull(workbenchWindow, "workbenchWindow is null. This is probably a bug and should be reported.");
+
+	final WorkflowEditor workflowEditor = (WorkflowEditor) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+		.getActivePage().getActiveEditor();
+	Validate.notNull(workflowEditor, "workflowEditor is null. This is probably a bug and should be reported.");
+
+	final WorkflowExportWizard wizard = new WorkflowExportWizard(workflowEditor,
+		KnimeWorkflowExporterProvider.getInstance().getWorkflowExporters(),
+		KnimeWorkflowExporterProvider.getInstance().getNodeConverters(),
+		KnimeWorkflowExporterProvider.getInstance().getSourceConverters());
+	final Shell parent = workbenchWindow.getShell();
+
+	try {
+	    wizard.validateWorkflowBeforeExport();
+	} catch (final Exception e) {
+	    // show an error window
+	    final IStatus status = new Status(IStatus.ERROR, KnimeWorkflowExporterActivator.PLUGIN_ID,
+		    "Workflow is not valid for conversion.");
+	    ErrorDialog.openError(parent, "Cannot convert workflow", e.getMessage(), status);
+	    return null;
+	}
+	final WizardDialog dialog = new WizardDialog(parent, wizard);
+	dialog.create();
+	dialog.getShell().setSize(Math.max(SIZING_WIZARD_WIDTH, dialog.getShell().getSize().x), SIZING_WIZARD_HEIGHT);
+	dialog.open();
+
+	// according to the javadoc, return value must be null
+	return null;
+    }
+}
